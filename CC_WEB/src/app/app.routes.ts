1e792d418c889c57ee7842ebcc6cbb1ccd317ec4
Hassam Arshad <hassamarshad021@gmail.com>
2025-10-31 12:20:12 +0100

    Angular project setup integrated Login and signup Page and navigation rendering towards the main page

--- CC_WEB/src/app/app.routes.ts
+++ CC_WEB/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./screens/login/login.component').then((m) => m.LoginComponent),
+    },
+    {
+        path: 'signup',
+        loadComponent: () =>
+            import('./screens/signup/signup.component').then((m) => m.SignupComponent),
+    },
+    { path: 'import-screen', loadComponent: () => import('./screens/import-screen/import-screen').then(m => m.ImportScreen) },
+
+
+    { path: '**', redirectTo: 'login' }
+];
