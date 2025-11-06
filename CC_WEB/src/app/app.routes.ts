fae632b639f9f207b92d246923ce553b343bf7cc
lovebkumar266 <lovebkumar266@gmail.com>
2025-11-06 10:41:27 +0100

    commit

--- CC_WEB/src/app/app.routes.ts
+++ CC_WEB/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ExportScreenComponent } from './screens/export-screen/export-screen';
+
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'export', pathMatch: 'full' },
   {
--- CC_WEB/src/main.ts
+++ CC_WEB/src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app';
+import { App } from './app/app';
 import { appRoutes } from './app/app.routes';
 
 console.log('🗺️ Registered routes:', appRoutes);
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
