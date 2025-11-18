c7cd9f9d05868ac002f4e4b87baaa45e021363a2
Hassam Arshad <hassamarshad021@gmail.com>
2025-11-18 12:45:45 +0100

    Feature Added: created general-details and applicant beneficiary screens for undertaking issuance

--- CC_WEB/src/app/screens/undertaking-issuance/components/application-beneficiary/application-beneficiary.ts
+++ CC_WEB/src/app/screens/undertaking-issuance/components/application-beneficiary/application-beneficiary.ts
@@ -1,11 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-application-beneficiary',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatIconModule,
+    MatRadioModule
+  ],
   templateUrl: './application-beneficiary.html',
   styleUrl: './application-beneficiary.scss',
 })
 export class ApplicationBeneficiary {
+  formGroup!: FormGroup;
+  isOpen: boolean = true; // default open
+  showAlternate: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.formGroup = this.fb.group({
+      applicantName: ['', Validators.required],
+      applicantAddress1: ['', Validators.required],
+      applicantAddress2: [''],
+      applicantAddress3: [''],
+
+      alternateName: [''],
+      alternateAddress1: [''],
+      alternateAddress2: [''],
+      alternateAddress3: [''],
+
+      beneficiaryName: ['', Validators.required],
+      beneficiaryAddress1: ['', Validators.required],
+      beneficiaryAddress2: [''],
+      beneficiaryAddress3: [''],
+      beneficiaryCountry: ['', Validators.required]
+    });
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  toggleAlternate() {
+    this.showAlternate = !this.showAlternate;
+  }
 }
---
+++ CC_WEB/src/app/screens/undertaking-issuance/components/bank-details/bank-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-details',
+  imports: [],
+  templateUrl: './bank-details.html',
+  styleUrl: './bank-details.scss',
+})
+export class BankDetails {
+
+}
