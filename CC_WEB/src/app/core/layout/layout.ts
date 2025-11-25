26cfd4fe2e64c32b2d534ba3403dfb7b362b956b
lovebkumar266 <lovebkumar266@gmail.com>
2025-11-25 12:50:19 +0100

    updated layout accordingly to Hassam branch and pushed to main

--- CC_WEB/src/app/core/layout/layout.ts
+++ CC_WEB/src/app/core/layout/layout.ts
@@ -6,6 +6,15 @@ import { MatIconModule } from "@angular/material/icon";
 import { TopbarComponent } from "../topbar/topbar";
 import { CommonModule } from '@angular/common';
 import { MatMenuModule } from "@angular/material/menu";
+import { filter } from 'rxjs/operators';
+
+interface MenuItem {
+  label: string;
+  icon?: string;
+  route?: string;
+  children?: MenuItem[];
+  open?: boolean;
+}
 
 @Component({
   selector: 'app-layout',
@@ -21,9 +30,9 @@ import { MatMenuModule } from "@angular/material/menu";
   ],
 })
 export class LayoutComponent implements OnInit {
-
   currentMenu: 'DEFAULT' | 'SYSTEM' | 'MIDDLE' = 'DEFAULT';
   collapsed = false;
+  shippingGuaranteeOpen = false;
   menuItems: MenuItem[] = [];
 
   systemOverviewMenu: MenuItem[] = [
@@ -79,8 +88,6 @@ export class LayoutComponent implements OnInit {
       ]
     }
   ];
-  importLcOpen = false;
-shippingGuaranteeOpen = false;
 
   constructor(private router: Router, private authService: AuthService) { }
 
@@ -153,186 +160,7 @@ shippingGuaranteeOpen = false;
       ];
     }
   }
-}
 
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { AuthService } from '../services/auth.service';
-// import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
-// import { MatIcon, MatIconModule } from "@angular/material/icon";
-// import { TopbarComponent } from "../topbar/topbar";
-// import { CommonModule } from '@angular/common';
-// import { MatMenuModule } from "@angular/material/menu";
-
-// interface MenuItem {
-//   label: string;
-//   icon?: string;
-//   route?: string;
-//   children?: MenuItem[];
-//   open?: boolean; // track collapsible state
-// }
-
-// @Component({
-//   selector: 'app-layout',
-//   templateUrl: './layout.html',
-//   styleUrls: ['./layout.scss'],
-//   imports: [CommonModule, RouterOutlet, TopbarComponent, MatIconModule, RouterLinkWithHref, MatMenuModule],
-// })
-// export class LayoutComponent implements OnInit {
-//   collapsed = false;
-//   menuItems: MenuItem[] = [];
-
-//   constructor(private router: Router, private authService: AuthService) { }
-
-//   ngOnInit() {
-//     const role = this.authService.getUserCategory();
-//     this.loadMenu(role);
-//   }
-
-//   toggleSidebar() {
-//     this.collapsed = !this.collapsed;
-//   }
-
-//   isCollapsed(): boolean {
-//     return this.collapsed;
-//   }
-
-//   loadMenu(role: 'ADMIN' | 'USER' | null) {
-//     if (role === 'ADMIN') {
-//       this.menuItems = [
-//         {
-//           label: 'System Overview', icon: 'insights', open: false, 
-//           children: [
-//             { label: 'Jurisdiction Maintenance', route: '/system-overview/jurisdiction-maintenance',  open: false, children: [
-//               { label: 'Roles', route: '/system-overview/jurisdiction-maintenance/role' },
-//               { label: 'Authorization', route: '/system-overview/jurisdiction-maintenance/authorization' },
-//             ] },
-//             { label: 'Customer Maintenance', route: '/system-overview/customer-maintenance',  open: false, children: [
-//               { label: 'Profiles', route: '/system-overview/customer-maintenance/customer-profile' },
-//               { label: 'Customer Entities', route: '/system-overview/customer-maintenance/customer-entities' },
-//               {
-//                 label: 'User Profile', route: '/system-overview/customer-maintenance/user-profile', 
-//               },
-//               { label: 'User Accounts', route: '/system-overview/customer-maintenance/user-accounts' },
-              
-//             ] },
-//             { label: 'User Maintenance', route: '/system-overview/user-maintenance' },
-//             { label: 'Change Profile', route: '/system-overview/profile' },
-//           ]
-//         },
-//         {
-//           label: 'Middle-Office', icon: 'group', open: false,
-//           children: [
-//             {
-//               label: 'Pending Approvals', route: '/middle-office/pending-approvals', open: false,
-//               children: [
-//                 { label: 'List of Records', route: '/middle-office/pending-approvals/list-of-records' },
-//                 { label: 'Edit Transaction', route: '/middle-office/pending-approvals/edit-transaction' },
-//                 { label: 'Retrieve Unsigned', route: '/middle-office/pending-approvals/retrieve-unsigned' },
-//                 { label: 'Pending Loans', route: '/middle-office/pending-approvals/pending-loans' },
-//                 { label: 'Approve / Reject', route: '/middle-office/pending-approvals/approve-reject' },
-//                 { label: 'Transactions', route: '/middle-office/pending-approvals/transactions' },
-//               ]
-//             },
-//             {
-//               label: 'Existing Records', route: '/middle-office/existing-records', open: false,
-//               children: [
-//                 { label: 'List of Records', route: '/middle-office/existing-records/list-of-records' },
-//                 { label: 'Edit Transaction', route: '/middle-office/existing-records/edit-transaction' },
-//                 { label: 'Retrieve Unsigned', route: '/middle-office/existing-records/retrieve-unsigned' },
-//               ]
-//             },
-//           ]
-//         },
-//         { label: 'Dashboard', icon: 'dashboard', route: '/admin-dashboard' },
-//         { label: 'Manage Users', icon: 'group', route: '/admin-users' },
-//         { label: 'Reports', icon: 'bar_chart', route: '/admin-reports' },
-//         { label: 'Settings', icon: 'settings', route: '/settings' },
-//       ];
-//     } else {
-//       this.menuItems = [
-//         { label: 'Dashboard', icon: 'dashboard', route: '/dashboard' },
-//         {
-//           label: 'Trade Services',
-//           icon: 'article',
-//           open: false,
-//           children: [
-//             {
-//               label: 'Import LC', route: '/import-welcome',
-//               open: false,
-//               children: [
-//                 { label: 'Create', route: '/import-screen' },
-//                 { label: 'Amend', route: '/import-screen/amend' },
-//               ],
-//             },
-//             { label: 'Export LC', route: '/export-screen' },
-//             {
-//               label: 'Undertaking Issuance', route: '/undertaking-welcome',
-//               open: false,
-//               children: [
-//                 { label: 'Create', route: '/undertaking-issuance' },
-//                 { label: 'Amend', route: '/undertaking-issuance/amend' },
-//               ],
-//             },
-//           ],
-//         },
-//         { label: 'Settings', icon: 'settings', route: '/settings' },
-//       ];
-//     }
-//   }
-
-//   // Toggle collapsible menus
-//   toggleMenu(item: MenuItem) {
-//     item.open = !item.open;
-//   }
-// }
-
-
-
-// import { Component } from '@angular/core';
-// import { Router, RouterOutlet, RouterLinkWithHref } from '@angular/router';
-// import { MatIcon, MatIconModule } from "@angular/material/icon";
-// import { TopbarComponent } from "../topbar/topbar";
-// import { CommonModule } from '@angular/common';
-// import { MatMenuModule } from "@angular/material/menu";
-
-// @Component({
-//   selector: 'app-layout',
-//   templateUrl: './layout.html',
-//   styleUrls: ['./layout.scss'],
-//   imports: [CommonModule, RouterOutlet, TopbarComponent, MatIconModule, RouterLinkWithHref, MatMenuModule],
-// })
-// export class LayoutComponent {
-//   collapsed = false;
-//   tradeMenuOpen = false;
-//   undertakingOpen = false;
-//   importLcOpen = false;
-
-//   constructor(private router: Router) { }
-
-//   toggleSidebar() {
-//     this.collapsed = !this.collapsed;
-//   }
-
-//   isCollapsed(): boolean {
-//     return this.collapsed;
-//   }
-
-//   toggleTradeMenu() {
-//     this.tradeMenuOpen = !this.tradeMenuOpen;
-//   }
-//   toggleUndertakingMenu() {
-//     this.undertakingOpen = !this.undertakingOpen;
-//   }
-//   toggleImportLcMenu() {
-//     this.importLcOpen = !this.importLcOpen;
-//   }
-// 
-  toggleImportLcMenu() {
-    this.importLcOpen = !this.importLcOpen;
-  }
 
   toggleShippingGuaranteeMenu() {
     this.shippingGuaranteeOpen = !this.shippingGuaranteeOpen;
