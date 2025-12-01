85f53f24868de4cdacf9702a8f9cb8e1b1f60ef4
lovebkumar266 <lovebkumar266@gmail.com>
2025-12-01 06:43:26 +0100

    dashboard feature change

--- CC_WEB/src/app/screens/USER/export-collection/export-collection.ts
+++ CC_WEB/src/app/screens/USER/export-collection/export-collection.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, FormArray } from '@angular/forms';
 
+// CHILD COMPONENTS
 import { GeneralDetails } from "../../USER/export-collection/components/general-details/general-details";
 import { DrawerDraweeDetails } from "../../USER/export-collection/components/drawer-drawee-details/drawer-drawee-details";
 import { BankDetailsComponent } from '../../USER/export-collection/components/bank-details/bank-details';
@@ -29,24 +30,108 @@ import { Sidebar } from "../../../core/sidebar/sidebar";
     AttachmentsDocuments,
     PreviewSectionComponent,
     Sidebar
-],
+  ],
   templateUrl: './export-collection.html',
   styleUrls: ['./export-collection.scss']
 })
-export class ExportCollectionComponent implements AfterViewInit {
+export class ExportCollectionComponent implements OnInit, AfterViewInit {
+
+  importForm!: FormGroup;
   currentStep = 0;
+  uploadedFiles: any[] = [];
 
   exportCollectionSteps = [
     { label: "General Details" },
-    {  label: "Drawer and Drawee Details" },
-    {  label: "Bank Details" },
-    {  label: "Payment and Account Details" },
-    {  label: "Shipping Details" },
-    {  label: "Licenses" },
-    {  label: "Collection Instructions" },
-    {  label: "Attachments and Documents" },
-    {  label: "Preview" }
+    { label: "Drawer and Drawee Details" },
+    { label: "Bank Details" },
+    { label: "Payment and Account Details" },
+    { label: "Shipping Details" },
+    { label: "Licenses" },
+    { label: "Collection Instructions" },
+    { label: "Attachments and Documents" },
+    { label: "Preview" }
   ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.importForm = this.fb.group({
+
+      generaldetails: this.fb.group({
+        collectionType: [''],
+        customerReference: [''],
+        draweeReference: ['']
+      }),
+
+      drawerdrawee: this.fb.group({
+        drawerName: [''],
+        drawerAddress1: [''],
+        drawerAddress2: [''],
+        draweeName: [''],
+        draweeAddress1: [''],
+        draweeAddress2: ['']
+      }),
+
+      bankdetails: this.fb.group({
+        remittingBankName: [''],
+        remittingIssuerRef: [''],
+        principalAccount: [''],
+        feeAccount: [''],
+        presentingBankName: [''],
+        presentingAddress1: [''],
+        presentingAddress2: [''],
+        collectingBankName: [''],
+        collectingSwiftCode: [''],
+        collectingReference: ['']
+      }),
+
+      paymentamount: this.fb.group({
+        amount: [''],
+        currency: [''],
+        paymentType: [''],
+        tenor: [''],
+        paymentReference: ['']
+      }),
+
+      shippingdetails: this.fb.group({
+        shippingMethod: [''],
+        shipmentReference: [''],
+        shippingFrom: [''],
+        shippingTo: [''],
+        shipmentDate: [''],
+        incotermsRules: [''],
+        incoterms: ['']
+      }),
+
+      license: this.fb.group({
+        licenseNumber: [''],
+        licenseDate: ['']
+      }),
+
+      collectioninstructions: this.fb.group({
+        advicePaymentBy: [''],
+        adviceAcceptanceDate: [''],
+        openingCharges: [''],
+        outsideCountryCharges: [''],
+        waiveCharges: [false],
+        protestNonPayment: [false],
+        protestNonAcceptance: [false],
+        adviceReasonRefusal: [''],
+        acceptanceDeferred: [false],
+        warehouseInsurance: [false],
+        referTo: ['']
+      }),
+
+      attachments: this.fb.group({
+        documents: this.fb.array([])
+      })
+    });
+  }
+
+  get documents(): FormArray {
+    return this.importForm.get('attachments.documents') as FormArray;
+  }
+
   ngAfterViewInit() {
     setTimeout(() => {
       const sections = document.querySelectorAll('section');
@@ -55,8 +140,7 @@ export class ExportCollectionComponent implements AfterViewInit {
         (entries) => {
           for (const entry of entries) {
             if (entry.isIntersecting) {
-              const index = Array.from(sections)
-                .indexOf(entry.target as HTMLElement);
+              const index = Array.from(sections).indexOf(entry.target as HTMLElement);
               this.currentStep = index;
             }
           }
@@ -71,21 +155,18 @@ export class ExportCollectionComponent implements AfterViewInit {
     }, 200);
   }
 
-  // Sidebar scroll
   scrollToSection(i: number) {
     this.currentStep = i;
     const section = document.getElementById(`section-${i}`);
     section?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 
-  // Next section
   next() {
     if (this.currentStep < this.exportCollectionSteps.length - 1) {
       this.scrollToSection(this.currentStep + 1);
     }
   }
 
-  // Previous section
   previous() {
     if (this.currentStep > 0) {
       this.scrollToSection(this.currentStep - 1);
