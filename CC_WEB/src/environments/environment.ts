2a435bbe53dd864314437cae6ba197ac5217a41e
Ameen-hp <devameen45@gmail.com>
2026-02-11 16:56:24 +0500

    changed the path of dashboard from app.routes , modified the enviroments

--- CC_WEB/src/environments/environment.ts
+++ CC_WEB/src/environments/environment.ts
@@ -3,7 +3,6 @@ export const environment = {
 
     // Settlement System via Gateway
     apiUrl: 'http://localhost:8050/settlementsystem/api/v1',
-
     // Middleware via Gateway
     apiURL_MIDDLEWARE: 'http://localhost:8050/middleware-service/mw/api/v1'
 };
